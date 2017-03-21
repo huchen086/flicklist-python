224336f3355aa164a22f628d8611edbbb08ed368
Hu Chen <huchen086@gmail.com>
2017-03-20 20:00:47 -0500

    random movies completed

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import webapp2
+import random
 
 class Index(webapp2.RequestHandler):
 
     def getRandomMovie(self):
 
         # TODO: make a list with at least 5 movie titles
+        movielist = ["Star Wars", "Forrest Gump", "The Godfather",
+        "The Shawshank Redemption", "Pupl Fiction", "The Matrix", "Fight Club",
+        "The Wizard of Oz", "E.T."]
 
         # TODO: randomly choose one of the movies, and return it
+        n = random.randint(0,len(movielist)-1)
 
-        return "The Big Lebowski"
+        return movielist[n]
 
     def get(self):
         # choose a movie by invoking our new function
@@ -20,6 +25,9 @@ class Index(webapp2.RequestHandler):
 
         # TODO: pick a different random movie, and display it under
         # the heading "<h1>Tommorrow's Movie</h1>"
+        movie2 = self.getRandomMovie()
+        content += "<h1>Tommorrow's Movie</h1>"
+        content += "<p>" + movie2 + "</p>"
 
         self.response.write(content)
 
